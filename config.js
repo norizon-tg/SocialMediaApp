6df76891dbc2291946fc6289c02cefcbe63a406a
norizon-tg <dizonnorman543@gmail.com>
2020-11-04 23:04:12 +0800

    Second Part of Setting Up Database and Server. Add gitignore to ignore node_modules and config.js for faster push and commits.

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGODB:'mongodb+srv://user0:8hCEBh7!G4R*@cluster0.ygdij.mongodb.net/merng?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const Post = require('./models/Post');
+const { MONGODB } = require('./config.js');
+
 const typeDefs = gql`
+    type Post{
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
     type Query{
-        sayHi: String!
+        getPosts: [Post]
     }
 `;
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello World'
+            async getPosts(){
+                try{
+                    const posts = await Post.find();
+                    return posts;
+                } catch(err){
+                    throw new Error(err);
+                }
+            }
         }
     };
 const server = new ApolloServer({
@@ -15,6 +33,11 @@ const server = new ApolloServer({
     resolvers
 });
 
-server.listen({ port: 5000}).then(res => {
-        console.log(`Server running at ${res.url}`);
-});
+mongoose
+    .connect(MONGODB, { useNewUrlParser: true})
+    .then(() =>{
+        console.log('MongoDB Connected');
+        return server.listen({ port: 5000});
+    }).then(res => {
+            console.log(`Server running at ${res.url}`);
+    });
